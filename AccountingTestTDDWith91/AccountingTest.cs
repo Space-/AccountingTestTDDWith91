a283198fcea4658b9366584f89c889225c434838
Bruce <gkm85663@gmail.com>
2019-12-22 17:11:35 +0800

    Refactor - remove unused variable and handle duplicate code

--- AccountingTestTDDWith91/AccountingTest.cs
+++ AccountingTestTDDWith91/AccountingTest.cs
@@ -7,7 +7,6 @@ namespace AccountingTestTDDWith91
 {
     public class AccountingTest
     {
-        private List<Budget> _budgets;
         private IRepository _budgetRepository;
         private Accounting _accounting;
 
@@ -21,35 +20,29 @@ namespace AccountingTestTDDWith91
         [Test]
         public void No_Budget()
         {
-            _budgets = new List<Budget>();
-            GivenBudget(_budgets);
-
-            _accounting = new Accounting(_budgetRepository);
-            var totalBudget = _accounting.QueryBudget(
-                new DateTime(2019, 4, 1), new DateTime(2019, 4, 1));
-
-            Assert.AreEqual(0, totalBudget);
-        }
-
-        private void GivenBudget(List<Budget> budgets)
-        {
-            _budgetRepository.GetAll().Returns(budgets);
+            GivenBudgets(new List<Budget>());
+            TotalBudgetShouldBe(0, new DateTime(2019, 4, 1), new DateTime(2019, 4, 1));
         }
 
         [Test]
         public void Period_Inside_Budget_Month()
         {
-            _budgets = new List<Budget>()
+            GivenBudgets(new List<Budget>
             {
                 new Budget() { YearMonth = "201904", Amount = 1 }
-            };
+            });
 
-            GivenBudget(_budgets);
+            TotalBudgetShouldBe(1, new DateTime(2019, 4, 1), new DateTime(2019, 4, 1));
+        }
 
-            var totalBudget = _accounting.QueryBudget(
-                new DateTime(2019, 4, 1), new DateTime(2019, 4, 1));
+        private void TotalBudgetShouldBe(int expected, DateTime startDate, DateTime endDate)
+        {
+            Assert.AreEqual(expected, _accounting.QueryBudget(startDate, endDate));
+        }
 
-            Assert.AreEqual(1, totalBudget);
+        private void GivenBudgets(List<Budget> budgets)
+        {
+            _budgetRepository.GetAll().Returns(budgets);
         }
     }
 }
